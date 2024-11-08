1da5ab113f587f820145ccf4bea24d0c1db04637
Thomas Rousseau <t.rousseau@liveencheres.com>
2024-11-08 22:59:29 +0100

    feat(User) add /me route

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const UserController = () => import('#controllers/user_controller')
 const AuthController = () => import('#controllers/auth_controller')
 
 router
@@ -17,6 +18,7 @@ router
         router.post('auth/register', [AuthController, 'register'])
         router.post('auth/login', [AuthController, 'login'])
         router.post('auth/logout', [AuthController, 'logout'])
+        router.get('/me', [UserController, 'me'])
       })
       .prefix('v1')
   })
