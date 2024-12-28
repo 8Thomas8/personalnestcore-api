f51452cb39e26e31c9716b54fce3e53cc99d5cdd
Thomas Rousseau <thomas.rousseau@proton.me>
2024-12-29 00:27:21 +0100

    feat(Support) support feature canceled

--- start/routes.ts
+++ start/routes.ts
@@ -9,8 +9,6 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
-const SupportResponseController = () => import('#controllers/support_response_controller')
-const SupportController = () => import('#controllers/support_controller')
 
 const UserController = () => import('#controllers/user_controller')
 const AuthController = () => import('#controllers/auth_controller')
@@ -25,11 +23,6 @@ router
         router.get('/me', [UserController, 'me']).use(middleware.auth())
         router
           .group(() => {
-            router.post('support', [SupportController, 'create'])
-            router.get('support', [SupportController, 'readAll'])
-            router.get('support/:id', [SupportController, 'readOne'])
-            router.put('support/:id', [SupportController, 'update']) // TODO IF ADMIN
-            router.post('support/response', [SupportResponseController, 'create'])
           })
           .use(middleware.auth())
       })
