0bf1c14d110e5308baaac39561f1a27f98a118ba
Thomas Rousseau <thomas.rousseau@proton.me>
2024-12-29 02:15:00 +0100

    models and validators added

---
+++ app/validators/user_drug.ts
@@ -0,0 +1,42 @@
+import vine from '@vinejs/vine'
+import { DrugForm, DrugUnit } from '../../types/constants.js'
+import { Database } from '@adonisjs/lucid/database'
+import { DateTime } from 'luxon'
+
+const transformDate = (value: string) => {
+  const date = DateTime.fromFormat(value, 'dd/MM/yyyy')
+  if (!date.isValid) {
+    throw new Error('Invalid date format. Expected format: dd/MM/yyyy')
+  }
+  return date
+}
+
+export const createUserDrugValidator = vine.compile(
+  vine.object({
+    drugBrandId:  vine.number().exists(async (db: Database, value: string) => {
+      return await db.from('drug_brands').where('id', value).first()
+    }),
+    drugNameId: vine.number().exists(async (db: Database, value: string) => {
+      return await db.from('drug_names').where('id', value).first()
+    }),
+    form: vine.enum(DrugForm),
+    dose: vine.number(),
+    unit: vine.enum(DrugUnit),
+    expirationDateTime: vine.string().transform(transformDate)
+  })
+)
+
+export const updateUserDrugValidator = vine.compile(
+  vine.object({
+    drugBrandId:  vine.number().exists(async (db: Database, value: string) => {
+      return await db.from('drug_brands').where('id', value).first()
+    }),
+    drugNameId: vine.number().exists(async (db: Database, value: string) => {
+      return await db.from('drug_names').where('id', value).first()
+    }),
+    form: vine.enum(DrugForm),
+    dose: vine.number(),
+    unit: vine.enum(DrugUnit),
+    expirationDateTime: vine.string().transform(transformDate)
+  })
+)
