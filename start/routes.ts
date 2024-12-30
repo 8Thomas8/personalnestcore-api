38718c3d9b8351f3fb14ae4f7545476cbb178236
Thomas Rousseau <thomas.rousseau@proton.me>
2024-12-30 01:28:25 +0100

    add routes

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,9 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
+import DrugBrand from '#models/drug_brand'
+import DrugName from '#models/drug_name'
+import UserDrug from '#models/user_drug'
 
 const UserController = () => import('#controllers/user_controller')
 const AuthController = () => import('#controllers/auth_controller')
@@ -23,6 +26,15 @@ router
         router.get('/me', [UserController, 'me']).use(middleware.auth())
         router
           .group(() => {
+            router.post('drug-brand', [DrugBrand, 'create'])
+            router.get('drug-brand', [DrugBrand, 'readAll'])
+            router.delete('drug-brand/:id', [DrugBrand, 'delete'])
+            router.post('drug-name', [DrugName, 'create'])
+            router.get('drug-name', [DrugName, 'readAll'])
+            router.delete('drug-name/:id', [DrugName, 'delete'])
+            router.post('user-drug', [UserDrug, 'create'])
+            router.get('user-drug', [UserDrug, 'readAll'])
+            router.delete('user-drug/:id', [UserDrug, 'delete'])
           })
           .use(middleware.auth())
       })
