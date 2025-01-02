20df6904798a8bceb594d4504d5d89e024a01c55
Thomas Rousseau <thomas.rousseau@proton.me>
2025-01-02 01:45:52 +0100

    fix(UserDrug) update for frontend (#10)

--- app/controllers/user_drug_controller.ts
+++ app/controllers/user_drug_controller.ts
@@ -1,18 +1,39 @@
 import { HttpContext } from '@adonisjs/core/http'
 import UserDrug from '#models/user_drug'
-import { createUserDrugValidator } from '#validators/user_drug'
+import { createUserDrugValidator, updateUserDrugQuantityValidator, updateUserDrugValidator } from '#validators/user_drug'
+import { DateTime } from 'luxon'
 
 export default class UserDrugController {
-  public readAll = async ({ auth, request, response }: HttpContext) => {
+  public readAll = async ({auth, request, response}: HttpContext) => {
     try {
       await auth.authenticate()
 
-      return response.ok(
-        await UserDrug.query()
-          .preload('drugBrand')
-          .preload('drugName')
-          .paginate(request.input('page', 1), 20)
-      )
+      const {itemPerPage, terms, expiredOnly, expireSoon} = request.qs()
+
+      const query = UserDrug.query().preload('drugBrand').preload('drugName')
+
+      if (terms) {
+        query
+          .whereHas('drugBrand', (brandQuery) => {
+            brandQuery.where('name', 'like', `%${terms}%`)
+          })
+          .orWhereHas('drugName', (nameQuery) => {
+            nameQuery.where('name', 'like', `%${terms}%`)
+          })
+          .orWhere('dose', 'like', `%${terms}%`)
+          .orWhere('form', 'like', `%${terms}%`)
+      }
+
+      if (expiredOnly === 'true') {
+        query.where('expirationDateTime', '<', DateTime.now().toISO())
+      }
+
+      if (expireSoon === 'true') {
+        const soonDate = DateTime.now().plus({days: 60}).toISO()
+        query.whereBetween('expirationDateTime', [DateTime.now().toISO(), soonDate])
+      }
+
+      return response.ok(await query.paginate(request.input('page', 1), itemPerPage ?? 20))
     } catch (error) {
       console.log(error)
       return response.badRequest({
@@ -22,13 +43,40 @@ export default class UserDrugController {
     }
   }
 
-  public create = async ({ auth, request, response }: HttpContext) => {
+  public create = async ({auth, request, response}: HttpContext) => {
     try {
-      const { drugBrandId, drugNameId, unit, form, dose, expirationDateTime, note } =
+      const {drugBrandId, drugNameId, unit, form, dose, expirationDateTime, note, quantity} =
         await request.validateUsing(createUserDrugValidator)
       await auth.authenticate()
 
-      await UserDrug.create({ drugBrandId, drugNameId, unit, form, dose, expirationDateTime, note })
+      const matchingUserDrugs: UserDrug[] = await UserDrug.query()
+        .where('drugBrandId', drugBrandId)
+        .andWhere('drugNameId', drugNameId)
+        .andWhere('unit', unit)
+        .andWhere('form', form)
+        .andWhere('dose', dose)
+        .exec()
+
+      const existingUserDrug = matchingUserDrugs?.some((userDrug) => {
+        return userDrug.expirationDateTime.toISO() === expirationDateTime.toISO()
+      })
+
+      if (existingUserDrug) {
+        return response.badRequest({
+          message: 'A similar user drug already exists',
+        })
+      }
+
+      await UserDrug.create({
+        drugBrandId,
+        drugNameId,
+        unit,
+        form,
+        dose,
+        expirationDateTime,
+        note,
+        quantity,
+      })
 
       return response.created({
         message: 'User drug created successfully',
@@ -42,7 +90,7 @@ export default class UserDrugController {
     }
   }
 
-  public delete = async ({ auth, request, response }: HttpContext) => {
+  public delete = async ({auth, request, response}: HttpContext) => {
     try {
       await auth.authenticate()
       const userDrug = await UserDrug.findOrFail(request.param('id'))
@@ -53,6 +101,63 @@ export default class UserDrugController {
         message: 'User drug deleted successfully',
       })
     } catch (error) {
+      return response.badRequest({
+        message: 'Request failed',
+        errors: error.messages,
+      })
+    }
+  }
+
+  public update = async ({auth, request, response}: HttpContext) => {
+    try {
+      await auth.authenticate()
+      const userDrug = await UserDrug.findOrFail(request.param('id'))
+
+      const {drugBrandId, drugNameId, unit, form, dose, expirationDateTime, note, quantity} =
+        await request.validateUsing(updateUserDrugValidator)
+
+      userDrug.merge({
+        drugBrandId,
+        drugNameId,
+        unit,
+        form,
+        dose,
+        expirationDateTime,
+        note,
+        quantity,
+      })
+
+      await userDrug.save()
+
+      return response.ok({
+        message: 'User drug updated successfully',
+      })
+    } catch (error) {
+      console.log(error)
+      return response.badRequest({
+        message: 'Request failed',
+        errors: error.messages,
+      })
+    }
+  }
+
+  public updateQuantity = async ({auth, request, response}: HttpContext) => {
+    try {
+      await auth.authenticate()
+      const userDrug = await UserDrug.findOrFail(request.param('id'))
+
+      const { quantity} =
+        await request.validateUsing(updateUserDrugQuantityValidator)
+
+      userDrug.quantity = quantity
+
+      await userDrug.save()
+
+      return response.ok({
+        message: 'User drug quantity updated successfully',
+      })
+    } catch (error) {
+      console.log(error)
       return response.badRequest({
         message: 'Request failed',
         errors: error.messages,
--- app/validators/drug_name.ts
+++ app/validators/drug_name.ts
@@ -1,5 +1,13 @@
 import vine from '@vinejs/vine'
 
+export const readAllDrugNameByDrugBrandIdValidator = vine.compile(
+  vine.object({
+    drugBrandId: vine.number().exists(async (db, value) => {
+      return await db.from('drug_brands').where('id', value).first()
+    }),
+  })
+)
+
 export const createDrugNameValidator = vine.compile(
   vine.object({
     name: vine
@@ -7,17 +15,14 @@ export const createDrugNameValidator = vine.compile(
       .trim()
       .minLength(2)
       .unique(async (db, value) => {
-        const name = await db
-          .from('drug_names')
-          .where('name', value)
-          .first()
+        const name = await db.from('drug_names').where('name', value).first()
         return !name
       }),
     drugBrandId: vine.number().exists(async (db, value) => {
       const brand = await db.from('drug_brands').where('id', value).first()
       return !!brand
-    })
-  }),
+    }),
+  })
 )
 
 export const updateDrugNameValidator = vine.compile(
@@ -27,11 +32,8 @@ export const updateDrugNameValidator = vine.compile(
       .trim()
       .minLength(2)
       .unique(async (db, value) => {
-        const name = await db
-          .from('drug_names')
-          .where('name', value)
-          .first()
+        const name = await db.from('drug_names').where('name', value).first()
         return !name
-      })
+      }),
   })
 )
