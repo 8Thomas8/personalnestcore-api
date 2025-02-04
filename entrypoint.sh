67a1400841bae72afe36d0f82c19972f3238285d
Thomas Rousseau <thomas.rousseau@proton.me>
2025-02-04 01:33:35 +0100

    fixup! feat(Dockerfile) docker container created

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-# Générer une nouvelle APP_KEY si elle n'est pas déjà définie
+# Generate a new APP_KEY if it is not already defined
 if [ -z "$APP_KEY" ]; then
     export APP_KEY=$(openssl rand -base64 32)
     echo "Generated APP_KEY: $APP_KEY"
 fi
 
-# Créer ou mettre à jour le fichier .env avec l'APP_KEY
+# Create or update the .env file with the APP_KEY
 echo "APP_KEY=$APP_KEY" > .env
 
 
 
-# Vérifie si les migrations sont nécessaires, puis exécute-les sans confirmation
+# Check if migrations are required, then run them without confirmation
 echo "Running migrations..."
 node ace migration:run --force || true
 
-# Lancer l'application
+# Start the application
 echo "Starting application..."
 exec node ./bin/server.js
