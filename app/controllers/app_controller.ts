b75655426f2a06afbf1caf721ec8478aa6497412
Thomas Rousseau <thomas.rousseau@proton.me>
2025-03-06 01:09:08 +0100

    feat(Version) no cache non version file

--- app/controllers/app_controller.ts
+++ app/controllers/app_controller.ts
@@ -7,6 +7,9 @@ export default class AppController {
     try {
       const jsonPath = join(process.cwd(), 'resources/version.json')
       const data = await fs.readFile(jsonPath, 'utf-8')
+      response.header('Cache-Control', 'no-cache, no-store, must-revalidate')
+      response.header('Pragma', 'no-cache')
+      response.header('Expires', '0')
       return response.ok(JSON.parse(data))
     } catch (error) {
       return response.internalServerError({ error: 'Failed to load JSON' })
