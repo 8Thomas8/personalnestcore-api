022a77bac45785130204d4186d46f587341bb398
Thomas Rousseau <thomas.rousseau@proton.me>
2025-03-29 00:12:54 +0100

    fix(CustomRecords) update validator

--- app/validators/custom-records/custom_records.ts
+++ app/validators/custom-records/custom_records.ts
@@ -16,17 +16,22 @@ export const createCustomRecordValidator = vine.compile(
   })
 )
 
-export const updateCustomRecordValidator = vine.compile(
-  vine.object({
-    name: vine
-      .string()
-      .trim()
-      .minLength(4)
-      .maxLength(25)
-      .unique(async (db, value) => {
-        const brand = await db.from('custom_records').where('name', value).first()
-        return !brand
-      }),
-    view: vine.enum(CustomRecordView).nullable(),
-  })
-)
+export const updateCustomRecordValidator = (recordId: number | string) =>
+  vine.compile(
+    vine.object({
+      name: vine
+        .string()
+        .trim()
+        .minLength(4)
+        .maxLength(25)
+        .unique(async (db, value) => {
+          const brand = await db
+            .from('custom_records')
+            .where('name', value)
+            .whereNot('id', recordId)
+            .first()
+          return !brand
+        }),
+      view: vine.enum(CustomRecordView).nullable(),
+    })
+  )
